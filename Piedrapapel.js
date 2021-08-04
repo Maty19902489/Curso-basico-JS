1e7e41a1d30b2f9bfe9dadd599b20867fd820939
Maty <roblesmatias31@gmail.com>
2021-08-04 00:46:25 -0300

    agregamos archivo piedrapapel

---
+++ Piedrapapel.js
@@ -0,0 +1,25 @@
+var jugador;
+var cpu;
+
+if (jugador == "tijeras" && cpu == "papel") 
+{
+    console.log = ("Jugador es el ganadror");
+} 
+
+else if
+
+(jugador == "papel" && cpu == "piedra") 
+{
+    console.log = ("Jugador es el ganador");
+} 
+
+else if (jugador == "piedra" && cpu == "tijeras") 
+{
+    console.log = ("Jugador es el ganadror");
+} 
+
+else if (jugador == cpu) 
+{
+    console.log = "empate"
+}
+else (console.log = "cpu es el ganador");
